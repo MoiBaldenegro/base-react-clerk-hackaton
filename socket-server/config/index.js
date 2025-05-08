4b492891969070fd811642ae418916ade9a7541f
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2025-05-08 17:03:15 -0600

    build: :rocket: Confure CORS midleware

--- socket-server/config/index.js
+++ socket-server/config/index.js
@@ -2,6 +2,6 @@ export const config = {
     PORT: process.env.PORT || 5700,
     JUDGE0_API_KEY: '7955fdc668msh74d297d610aeed4p1b4696jsnfce4ad9db5da',
     JUDGE0_HOST: 'judge0-ce.p.rapidapi.com',
-    CORS_ORIGIN: process.env.CORS_ORIGIN || ['http://localhost:5173', "http://base-react-clerk-hackaton-3it7jy75q-tomatepvs-projects.vercel.app", "https://midu.moibaldenegro.com"]
+    CORS_ORIGIN: process.env.CORS_ORIGIN || ['http://localhost:5173', "https://base-react-clerk-hackaton-git-main-tomatepvs-projects.vercel.app/", "https://midu.moibaldenegro.com"]
   };
   
